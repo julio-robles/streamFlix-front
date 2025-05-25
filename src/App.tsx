ce7c35a09a3e80d662d3c50af0fd72b641cdcc7f
julio-robles <julio.robles.9997@gmail.com>
2025-05-26 01:03:03 +0200

    Inicio curso

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import MovieCard from './components/MovieCard';
 import './App.css'
 
 function App() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Película destacada</h1>
-      <MovieCard id={550} /> {/* 550 = Fight Club en TMDb */}
-    </div>
+    <h1>SteamFlix</h1>
   );
 }
 
--- src/schemas/movie.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const MovieSchema = z.object({
-  id: z.number(),
-  title: z.string(),
-  release_date: z.string()
-});
-
-export type Movie = z.infer<typeof MovieSchema>;
